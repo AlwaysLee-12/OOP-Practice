bfd82a276e46a1cf6271db126f403e739cc621db
yeonjunglee <yeonjunglee@hyundai-autoever.com>
2024-11-11 15:12:01 +0900

    할인 정책 변경 메서드 생성

--- src/main/java/hello/oop/moviereservation/Movie.java
+++ src/main/java/hello/oop/moviereservation/Movie.java
@@ -29,4 +29,10 @@ public class Movie {
 
         return fee.minus(discountPolicy.calculateDiscountAmount(screening));
     }
+
+    //이처럼 상속을 사용했을 때보다 합성을 사용했을 때, 유연하게 구현 객체를 변경할 수 있다
+    //합성: 인터페이스에 정의된 메세지를 통해서만 코드를 재사용(구현에 대한 캡슐화 및 유연한 설계 가능)
+    public void changeDiscountPolicy(DiscountPolicy discountPolicy) {
+        this.discountPolicy = discountPolicy;
+    }
 }
